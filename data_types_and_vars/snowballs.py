208c837cbd8d4f21cca9d2ce7e803c8b4c89a7d1
Ivan Krumov <zakz13@gmail.com>
2022-01-23 19:07:12 +0200

    Edit dict sorting

--- data_types_and_vars/snowballs.py
+++ data_types_and_vars/snowballs.py
@@ -1,22 +1,19 @@
 import collections
 
 number_of_balls = int(input())
-weight = 0
-time_needed = 0
-quality = 0
-list_of_balls = []
 dict_balls = {}
-sorted_dict = {}
 ball_data = ""
 
-for i in range(number_of_balls):
+for i in range(1, number_of_balls+1):
     weight = int(input())
     time_needed = int(input())
     quality = int(input())
-    value = int(((weight / time_needed) ** quality))
-    ball_data = f"{weight} : {time_needed} = {value} ({quality})"
+    value = int((weight / time_needed) ** quality)
+    ball_data = f'{weight} : {time_needed} = {value} ({quality})'
     dict_balls[value] = ball_data
 
 # sorted(dict_balls.values())
-print(sorted(dict_balls.values())[0])
+od = collections.OrderedDict(sorted(dict_balls.items(), reverse=True))
+first_value = list(od.values())[0]
+print(first_value)
 
